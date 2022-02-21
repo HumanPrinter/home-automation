9759d700106c5f6edea11edb46411642333bcaa7
Oscar Brouwer <o_brouwer@hotmail.com>
2022-02-21 08:22:02 +0100

    Add the App ID of the service principals to the outputs

--- terraform/service_principals/outputs.tf
+++ terraform/service_principals/outputs.tf
@@ -1,7 +1,17 @@
+output "sp_azuredevops_appid" {
+  description = "The generated App ID for the Azure DevOps pipeline service principal"
+  value       = azuread_application.azuredevops.application_id
+}
+
 output "sp_azuredevops_password" {
   sensitive   = true
   description = "The generated password for the Azure DevOps pipeline service principal"
   value       = azuread_service_principal_password.azuredevops.value
+}
+
+output "sp_raspberrypi_appid" {
+  description = "The generated App ID for the Raspberry Pi service principal"
+  value       = azuread_application.raspberrypi.application_id
 }
 
 output "sp_raspberrypi_password" {
