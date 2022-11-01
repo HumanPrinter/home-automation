d1af1c1c1a0f4de1afc32cb5e1fa345f36c21d1b
Oscar Brouwer <o_brouwer@hotmail.com>
2022-11-01 11:10:58 +0100

    Updated Terraform providers

--- terraform/key_vault/configuration.tf
+++ terraform/key_vault/configuration.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">=1.1.0"
+  required_version = ">=1.3.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=3.25.0"
+      version = ">=3.29.0"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">=2.29.0"
+      version = ">=2.30.0"
     }
   }
 
